25bf27df19acfb82d7c6db08412c1942dceabaa8
George Raafat <georgexraafat@gmail.com>
2024-03-11 20:45:39 +0200

    doing some task

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -9,18 +9,15 @@ class FilesController {
     const token = req.header('X-Token');
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
-    const users = dbClient.db.collection('users');
-    const objectUserId = new ObjectID(userId);
-    const user = await users.findOne({ _id: objectUserId });
-    if (!user) {
+    if (!userId) {
       res.status(401).json({ error: 'Unauthorized' });
       return;
     }
     const {
       name,
       type,
-      parentId,
-      isPublic,
+      parentId = 0,
+      isPublic = false,
       data,
     } = req.body;
 
@@ -40,12 +37,12 @@ class FilesController {
       name,
       type,
       userId,
-      parentId: parentId || 0,
-      isPublic: isPublic || false,
+      parentId,
+      isPublic,
     };
     const files = dbClient.db.collection('files');
-    const _id = new ObjectID(parentId);
     if (parentId) {
+      const _id = new ObjectID(parentId);
       await files.findOne({ _id }, async (err, result) => {
         if (!result) {
           res.status(400).json({ error: 'Parent not found' });
@@ -55,28 +52,19 @@ class FilesController {
       });
     }
     if (type === 'folder') {
-      const result = await dbClient.db.collection('files').insertOne(file);
-      res.status(201).json({ ...file, id: result.insertedId });
+      const result = await files.insertOne({ ...file });
+      res.status(201).json({ ...file, id: result.insertedId.toString() });
+      return;
     }
     const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath, { recursive: true });
-    }
+    await fs.promises.mkdir(folderPath, { recursive: true });
     const filePath = `${folderPath}/${v4()}`;
-    fs.writeFile(
-      filePath,
-      Buffer.from(data, 'base64'),
-      'utf-8',
-      async (err) => {
-        if (!err) {
-          const result = await files.insertOne({
-            ...file,
-            localPath: filePath,
-          });
-          res.status(201).json({ ...file, id: result.insertedId });
-        }
-      },
-    );
+    await fs.promises.writeFile(filePath, Buffer.from(data, 'base64'));
+    const result = await files.insertOne({
+      ...file,
+      localPath: filePath,
+    });
+    res.status(201).json({ ...file, id: result.insertedId.toString() });
   }
 }
 module.exports = FilesController;
