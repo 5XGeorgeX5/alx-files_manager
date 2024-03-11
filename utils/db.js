bccb5bfb5f142c64cd1d36c24d61e3ae8009abac
George Raafat <georgexraafat@gmail.com>
2024-03-11 21:00:43 +0200

    doing some task

--- utils/db.js
+++ utils/db.js
@@ -1,29 +1,29 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-  constructor () {
+  constructor() {
     this.host = process.env.DB_HOST || 'localhost';
     this.port = process.env.DB_PORT || '27017';
     this.database = process.env.DB_DATABASE || 'files_manager';
     this.client = new MongoClient(
       `mongodb://${this.host}:${this.port}/${this.database}`,
-      { useUnifiedTopology: true }
+      { useUnifiedTopology: true },
     );
     this.client.connect();
     this.db = this.client.db(this.database);
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.isConnected();
   }
 
-  async nbUsers () {
+  async nbUsers() {
     const users = this.client.db(this.database).collection('users');
     const result = await users.countDocuments();
     return result;
   }
 
-  async nbFiles () {
+  async nbFiles() {
     const users = this.client.db(this.database).collection('files');
     const result = await users.countDocuments();
     return result;
